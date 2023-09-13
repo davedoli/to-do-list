5e50427d1028dbf2c586627ce0b641131ada3cc7
David Li <Li_Dong@bah.com>
2023-09-13 16:12:09 -0400

    added custom user model

--- to_do/task/models.py
+++ to_do/task/models.py
@@ -1,6 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class Task(models.Model):
-    posted_date = models.DateField()
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
